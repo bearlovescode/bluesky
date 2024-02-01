9541a72184c3b7018b0df3abd012999ab385da6e
Nic Barr <code@nicholasbarr.net>
2024-02-01 12:47:01 -0600

    wip checkpoint: working through the createSession flow

---
+++ src/Services/PostService.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Services;
+
+    use Bearlovescode\Bluesky\Services\Service;
+    use Bearlovescode\Datamodels\Dto\Dto;
+
+    class PostService extends Service
+    {
+        public function post(Dto $data)
+        {
+
+        }
+    }
